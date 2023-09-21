08577d08e04149b882e8b0c84e4e3a4cf7e9685a
ankit.kumar-at-527579388740 <ankit.kumar@borderfree.io>
2023-09-21 09:26:12 +0530

    cors-fix

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8006;
 app.use(bodyParser.json());
 const cors = require("cors");
+
+// Enable pre-flight requests for all routes
+app.options("*", cors());
+
+const pdfMailer = require("./routes/PdfMailer");
+const connectDB = require("./config/ConnectDB");
+
 app.use(
   cors({
     origin: "*",
   })
 );
-const pdfMailer = require("./routes/PdfMailer");
-const connectDB = require("./config/ConnectDB");
 
 app.use("/pdf-mailer", pdfMailer);
 
 app.listen(PORT, async () => {
   try {
     await connectDB();
-    console.log("db connection sucess !");
+    console.log("db connection success!");
   } catch (err) {
     console.log("getting error", err);
   }
