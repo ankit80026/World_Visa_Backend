c653b2860f947f70ac100c5c8aeaa42b0b1db41e
ankit.kumar-at-527579388740 <ankit.kumar@borderfree.io>
2023-09-21 13:52:52 +0530

    getting-cors-error

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8006;
 const cors = require("cors");
-
-const newURL = "https://world-visa-travel.vercel.app/moreinfo/officialvisa";
-app.use(
-  cors({
-    origin: newURL,
-  })
-);
+app.use(cors());
 
 const pdfMailer = require("./routes/PdfMailer");
 const connectDB = require("./config/ConnectDB");
